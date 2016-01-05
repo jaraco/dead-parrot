cb1e1fca023fa5ac312baab99ed15abcbd906385
Jason R. Coombs <jaraco@jaraco.com>
2016-01-05 18:05:53 -0500

    Use requests and portend for Python 3 compatibilty and simplicity of test.

--- uptests/web/responding.py
+++ uptests/web/responding.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-import urllib2
 import argparse
+
+import requests
+import portend
 
 parser = argparse.ArgumentParser()
 parser.add_argument('host')
 parser.add_argument('port', type=int)
 args = parser.parse_args()
+portend.occupied(args.host, args.port, timeout=10)
 
 root = 'http://{host}:{port}/'.format(**vars(args))
-try:
-	urllib2.urlopen(root)
-except urllib2.HTTPError as exc:
-	# parrot returns 404 for /
-	assert exc.code == 404
+resp = requests.get(root)
+# parrot returns 404 for /
+assert resp.status_code == 404
