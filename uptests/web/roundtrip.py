a6727b0daf7f0805c1558c6e3510c53b2946b65e
Jason R. Coombs <jaraco@jaraco.com>
2016-01-05 18:15:39 -0500

    remove stray comma

--- uptests/web/roundtrip.py
+++ uptests/web/roundtrip.py
@@ -15,7 +15,7 @@ root = 'http://{host}:{port}/'.format(**vars(args))
 data = "dparrot uptest"
 headers={
 	'Content-Type': 'text/plain',
-},
+}
 resp = requests.post(root, data=data, headers=headers)
 assert resp.headers['Access-Control-Allow-Origin'] == '*'
 new_url = resp.json()['url']
