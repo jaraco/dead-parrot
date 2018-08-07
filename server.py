58ad93eb91a6530985d4ec723c1e7cc62df18763
Allan Crooks <allan.crooks@sixtyten.org>
2018-08-07 14:25:04 +0100

    Drop environment production

--- server.py
+++ server.py
@@ -43,7 +43,6 @@ class DeadParrot:
             'global': {
                 'server.socket_host': '::0',
                 'server.socket_port': int(os.environ.get('PORT', 8080)),
-                'environment': 'production',
             },
             '/': {
                 'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
